5553e2d80bc688da821d549ae26de6c8e807f605
unknown <tejas.savalia7@gmail.com>
2018-10-05 11:50:21 -0400

    Codes for the lab

---
+++ Generating BOLD and fitting IEM/fit_IEM_Scolari.m
@@ -0,0 +1,236 @@
+% function fit_IEM_Scolari(p, BOLD_data)
+
+%BOLD_data is [nTrials nVox] matrix in which rows are orients 1-9 stacked on top of each other nPresentations times, stacked nRuns times
+    % nTrials is likely 288 (8 runs of 4 presentations of 9 orientations)
+subtract_mean_chan = 0; % 0 or 1
+
+
+m.a = 1; % channel amplitude
+m.b = 0; % channel baseline (additive constant)
+m.sinPower = 30; % with sinPower above about 8 or 10, the channels are narrow enough to NOT get rank deficient matrix
+m.x = linspace(0, pi-pi/(p.nOrients), p.nOrients);
+m.cCenters = linspace(0, pi-pi/p.nChans, p.nChans);%  + 0.5*pi/p.nOrients;
+C = zeros(p.nOrients*(p.nRuns-2), p.nChans);
+% C = zeros(p.nOrients*(p.nRuns-1), p.nChans);
+PosOffsetData = zeros(p.nSubs,p.nChans);
+NegOffsetData = zeros(p.nSubs,p.nChans);
+
+% C2_unshifted = zeros(p.nSubs,p.nTrials,p.nChans);
+% W_unshifted = zeros(p.nSubs,p.nOrients*p.nRuns/2,p.nVox);
+
+figure(9)
+clf(9)
+hold on
+%make design matrix (with channels - same number as Ho et al.)
+for ii=1:p.nChans
+    m.u = p.cCenters(ii);
+    % HALF SIN WAVE RAISED TO POWER 6
+    resp = m.a * sin(mod((m.x-m.u+pi/2),pi)).^m.sinPower + m.b;
+%     pred = p.a * exp(-(mod((p.x-p.u+pi/2),pi)-pi/2).^2 / (2*p.sig^2)) + p.b;
+    plot([1:9], resp)
+    C(:,ii) = repmat(resp', (p.nRuns-2),1);      
+%     C(:,ii) = repmat(resp', (p.nRuns-1),1);      
+end
+title('Channels, C1')
+
+for sub = 1:p.nSubs
+
+    %split data into training and test
+%     trn = p.data(sub).voxResp; %[nTrials nVox]
+    rCnt=1;
+    
+    if sub == 1
+        meanPosOffset = mean(mean(p.data(sub).voxResp(p.g==1,:)));
+        meanNegOffset =  mean(mean(p.data(sub).voxResp(p.g==2,:)));
+    end
+    
+    for run = 1:p.nRuns/2 % just go through runs 1 - 4, then hold out run and run+nRuns/2, so that 2 runs held out each time
+                            % note this ensures that 1 PosOffset and 1 NegOffset run are held out, but only if the data are ordered with all runs of one type in first half of runs
+        trnSet = ones(size(p.or));
+        trnSet(p.runs==run) = 0;
+        trnSet(p.runs==run+p.nRuns/2) = 0;
+
+        trn = p.data(sub).voxResp(trnSet == 1,:);     % data from training scans
+        tst = p.data(sub).voxResp(trnSet == 0,:);     % data from test scan
+        trnor = p.or(trnSet == 1);
+        tstor = p.or(trnSet == 0);
+        trns = p.runs(trnSet == 1);
+        tsts = p.runs(trnSet == 0);  
+        trng = p.g(trnSet == 1);
+        tstg = p.g(trnSet == 0);
+
+        % first, average over all like orientations in the training set from each run.
+        % stack them on top of each other
+        uRuns = unique(trns)';
+        tmp = zeros(p.nOrients*length(uRuns), size(trn,2));
+        sCnt = 1;
+        for ss=uRuns
+            for ii=1:p.nOrients
+                tmp((sCnt-1)*p.nOrients+ii,:) = nanmean(trn(trns==ss & trnor==ii,:));
+            end
+            sCnt = sCnt+1;
+        end
+        trnData = tmp;
+        tstData = tst;
+
+        % compute voxel-channel weights (do this jointly for speed and accuracy corr/incorr conditions)
+        % as estimated based on the training set.
+        w = C\trnData;   
+
+        % weights only zigzag when sin power is ~8 or less (and also they give nans for x (C2) in that case )
+
+        % these weights are arbitrary to an additive constant... only the shape of the function matters
+        % NOTE: we invert the weights simultaneously for all trials, but it would be the same if each trial done separately
+        x = inv(w*w')*w*tstData';
+%         x = x - nanmean(x(:));% + nanmean(tst(:));
+        chan = x';
+
+        if rCnt==1
+            C2 = [];
+            task = [];
+            overOr = [];
+            W = [];
+    %         PosOffsetMean=[];
+    %         NegOffsetMean=[];
+        end
+
+        % concatenate the channel responses from this run
+        C2 = [C2; chan];
+        W = [W; w];
+
+        % save the means of each condition
+    %     PosOffsetMean(rCnt) = nanmean(nanmean(tst(tstg==1,:)));
+    %     NegOffsetMean(rCnt) = nanmean(nanmean(tst(tstg==2,:)));
+
+        task = [task; tstg];
+        overOr = [overOr; tstor];
+        
+        %predict the test BOLD response (and see how it matches up)
+        predictedBOLD = chan*w; %gives a 36 * 100 matrix (nTestTrials * nVox)
+        predictedPosOffset = predictedBOLD(tstg==1,:);
+        predictedNegOffset = predictedBOLD(tstg==2,:);
+        fprintf('Subject %d\n', sub);
+        mean_predPosOffset = mean(mean(predictedPosOffset));
+        mean_predNegOffset = mean(mean(predictedNegOffset));
+        mean_testBOLDPosOffset = mean(mean(tstData(tstg==1)));
+        mean_testBOLDNegOffset = mean(mean(tstData(tstg==2)));
+        
+
+        rCnt=rCnt+1;
+    end
+
+    % circular shift
+    % recenter the response matrix (each row centered on the stimulus on that trial)
+    for ii=1:size(C2,1)
+       C2_centered(ii,:) = wshift('1D', C2(ii,:), overOr(ii)-ceil(p.nOrients/2));
+    end
+
+    PosOffsetVTF=(nanmean(C2_centered(task==1, :)));%;+nanmean(lowMean);
+    NegOffsetVTF=(nanmean(C2_centered(task==2, :)));%+nanmean(highMean);
+
+    PosOffsetData(sub,:) = PosOffsetVTF;
+    NegOffsetData(sub,:) = NegOffsetVTF;
+%     
+%     C2_unshifted(sub,:,:) = C2;
+%     W_unshifted(sub,:,:) = W;
+
+end
+% 
+
+figure(10), clf, hold on
+title('Full VTFs')
+x_axis = (-80:20:90)';
+x_axis = repmat(x_axis, 1, 2);
+d=[]; sem=[];
+if subtract_mean_chan == 0
+    d(:,1) = mean(NegOffsetData)';    
+    sem(:,1) = (std(NegOffsetData)./sqrt(size(NegOffsetData,1)))';
+    
+    d(:,2) = mean(PosOffsetData)';    
+    sem(:,2) = (std(PosOffsetData)./sqrt(size(PosOffsetData,1)))';
+    
+elseif subtract_mean_chan ==1  %PLOT CHAN RESPS WITH MEAN SUBTRACTED
+
+    NegOffsetData_mean_subtracted = NegOffsetData - mean(NegOffsetData(:)); 
+    d(:,1) = mean(NegOffsetData_mean_subtracted)';  
+    sem(:,1) = (std(NegOffsetData_mean_subtracted)./sqrt(size(NegOffsetData_mean_subtracted,1)))';
+
+    PosOffsetData_mean_subtracted = PosOffsetData - mean(PosOffsetData(:));  
+    d(:,2) = mean(PosOffsetData_mean_subtracted)';
+    sem(:,2) = (std(PosOffsetData_mean_subtracted)./sqrt(size(PosOffsetData_mean_subtracted,1)))';
+end
+  
+% plot(x_axis, d, 'o-', 'LineWidth', 2, 'MarkerSize', 10);
+errorbar(x_axis, d, sem, 'o-', 'LineWidth', 2, 'MarkerSize', 10);
+
+set(gca, 'FontSize', 20);
+set(gca, 'XLim', [x_axis(1)-5, x_axis(end)+5]);
+set(gca, 'XTick', [-80:40:90]);
+plot(get(gca, 'XLim'), [0,0], 'k-', 'LineWidth', 2)        
+xlabel('Offset from stimulus <deg>')
+ylabel('Channel response (a.u.)')
+legend({'NegOffset Trials', 'PosOffset Trials'})
+legend boxoff
+
+% next do the folded VTFs on each trial type (low contrast, high contrast)
+%fold the VTF curve in half like a calzone
+x_axis = (0:20:90)';
+x_axis = repmat(x_axis, 1, 2);
+
+% tmp = (fliplr(PosOffsetData(:,1:4))+PosOffsetData(:,6:9))./2;
+% PosOffsetDataC = [PosOffsetData(:,5), tmp];% concatenate the central datapoint (5), with the 'folded' data from 1:4 and 6:9
+% tmp = (fliplr(NegOffsetData(:,1:4))+ NegOffsetData(:,6:9))./2;
+% NegOffsetDataC = [NegOffsetData(:,5), tmp];
+
+tmp = (fliplr(NegOffsetData(:,1:4))+PosOffsetData(:,6:9))./2;
+CuedDirectionDataC = [PosOffsetData(:,5), tmp];
+tmp = (fliplr(PosOffsetData(:,1:4))+ NegOffsetData(:,6:9))./2;
+UncuedDirectiontDataC = [NegOffsetData(:,5), tmp];
+
+figure(11), clf, hold on
+title('Collapsed VTFs')
+d=[]; sem=[];
+
+if subtract_mean_chan == 0    
+    d(:,1) = mean(UncuedDirectiontDataC)';  %lowDataC is the folded data (two halves of the curve collapsed into one)
+    sem(:,1) = (std(UncuedDirectiontDataC)./sqrt(size(UncuedDirectiontDataC,1)))';
+   
+    d(:,2) = mean(CuedDirectionDataC)';    
+    sem(:,2) = (std(CuedDirectionDataC)./sqrt(size(CuedDirectionDataC,1)))';
+elseif subtract_mean_chan ==1  %PLOT CHAN RESPS WITH MEAN SUBTRACTED
+    d(:,1) = mean(UncuedDirectiontDataC)' - mean(UncuedDirectiontDataC(:));  
+    sem(:,1) = (std(UncuedDirectiontDataC)./sqrt(size(UncuedDirectiontDataC,1)))';
+    
+    d(:,2) = mean(CuedDirectionDataC)' - mean(CuedDirectionDataC(:));
+    sem(:,2) = (std(CuedDirectionDataC)./sqrt(size(CuedDirectionDataC,1)))';    
+end
+      
+errorbar(x_axis, d, sem, 'o-', 'LineWidth', 2, 'MarkerSize', 10);
+
+set(gca, 'FontSize', 20);
+set(gca, 'XLim', [x_axis(1)-5, x_axis(end)+5]);
+set(gca, 'XTick', [0:40:90]);
+%         set(gca, 'YLim', [-.25, 1.5])
+%         set(gca, 'YTick', [-.25:.25:1.5]);
+plot(get(gca, 'XLim'), [0,0], 'k-', 'LineWidth', 2)        
+xlabel('Offset from stimulus abs(<deg>)')
+ylabel('Channel response (a.u.)')
+legend({'Uncued Direction', 'Cued Direction'})
+legend boxoff         
+
+
+figure(12)
+clf(12)
+plot([1:9], w(:,1))
+hold on
+plot([1:9], w(:,4), 'r')
+plot([1:9], w(:,10), 'k')
+plot([1:9], w(:,16), 'c')
+plot([1:9], w(:,26), 'm')
+plot([1:9], w(:,57), 'g')
+plot([1:9], w(:,91), 'b')
+plot([1:9], w(:,85), '-o')
+title('Sample of Voxel Weights')
+
+% attempt to replicate Liu et al. ? 
+% (adding different levels of noise, and using different channel and NTF widths)
---
+++ Generating BOLD and fitting IEM/simulate_BOLD_Scolari.m
@@ -0,0 +1,234 @@
+% function simulate_BOLD_Scolari(voxNoiseRatio, NTFbaseline, NTFamp, NTFsig)
+
+% incorrect is just a regular set of neural resps; correct --> take the off-channel neurons and give them gain (for every stim)
+
+% NTFsig of 0.35 with notchwidth of 8 and gainFactor of 4 gives almost hte right shape (if you subtract the mean chan resp)
+
+% default parameters (that will be manipulated)
+voxNoiseScale = .5;
+NTFbaseline = 0;
+p.NTFamp = 1;
+p.NTFsig = .35 
+% GainFactor = [1.5 2]; % medium and high factors by which NTFs are multiplied for 'correct' trials (in off-channels)
+% parameters of simulation (not usually changed)
+normedNTF = 1;
+p.notchwidth = 8; %must be an even number. governs width of off-channel 'notch' that undergoes gain. higher is narrower.
+p.gainFactor = 2; %must be  <1 if using method 2 (slope of NTF) but >1 if using method 1 (sin wave function) see splice_NTFresponses function
+p.nVox = 100;
+p.nNTFs = 180;
+p.stimOrient = [0 20 40 60 80 100 120 140 160];% 200  240 280 320 360]'; % or add in that each is shown plus or minus 5 deg?
+p.nOrients = length(p.stimOrient);
+p.nRuns = 8;
+p.nPresentations = 4;
+% p.nTrialsPerOrient = p.nRuns*p.nPresentations; % 4 per run and 8 runs, as per Ho et al. 2012
+p.nTrials = p.nOrients*p.nPresentations*p.nRuns;
+p.nSubs = 500;
+x = linspace(0, pi-pi/(p.nOrients), p.nOrients);
+% NTFcenters = [1:180]';
+p.NTFcenters = linspace(0, pi-pi/p.nNTFs, p.nNTFs);
+
+for sub = 1:p.nSubs
+    voxelWeights = rand(p.nNTFs,p.nVox); % sampled as in Liu et al. (uniform dist)
+    for vox = 1:p.nVox
+        voxelWeights(:,vox) = voxelWeights(:,vox)/sum(voxelWeights(:,vox)); %normalized so each voxel's weights sum to 1
+    end
+
+    NTFresponses_Baseline = zeros(p.nOrients,p.nNTFs); % baseline resp of every NTF to every stim 
+    NTFresponses_Gain = zeros(p.nOrients,p.nNTFs); % 'gained' resp of every NTF to every stim
+    voxelResponses = zeros(p.nOrients*p.nPresentations, p.nVox, p.nRuns); % resp of every vox to every stim on every run
+    p.nChans = 9;
+
+    if sub==1
+        figure(1)
+        clf(1)
+        hold on
+    end
+    % calculate NTF responses
+    for NTF = 1:p.nNTFs
+        if normedNTF ==1 % normalize area under the NTF
+            NTFresponses_Baseline(:,NTF) = (1/sqrt(2*pi*(NTFsig)^2)) * p.NTFamp * exp(-(mod((x-p.NTFcenters(NTF)+pi/2),pi)-pi/2).^2 / (2*p.NTFsig^2)) + NTFbaseline;
+            if sub==1 && mod(NTF,20)==1, plot([1:9], NTFresponses_Baseline(:,NTF)); end
+        elseif normedNTF==0 % do not normalize
+            NTFresponses_Baseline(:,NTF) = p.NTFamp * exp(-(mod((x-p.NTFcenters(NTF)+pi/2),pi)-pi/2).^2 / (2*p.NTFsig^2)) + NTFbaseline;
+            if sub == 1 && mod(NTF,20)==1, plot([1:9], NTFresponses_Baseline(:,NTF)); end
+        end
+    end
+    
+    if sub == 1
+        title('NTFresponses Baseline') 
+
+        figure(2)
+        clf(2)
+        imagesc(NTFresponses_Baseline);
+        colorbar;
+        title('NTFresponses Baseline');
+
+
+        fig_num = 3;
+        clf(fig_num)
+        figure(fig_num)
+        plot([1:180],NTFresponses_Baseline(3,:))
+        title('Orient #3 Responses across all NTFs');
+    end
+
+    or = zeros(p.nOrients*p.nPresentations,1); %,p.nRuns); % [ 9*4 ] , [8]
+    stimPres = zeros(p.nOrients*p.nPresentations,1); %,p.nRuns);
+    g = zeros(p.nOrients*p.nPresentations,p.nRuns);
+    %% calculate voxel responses
+    % voxelResponses = zeros(p.nVox, p.nOrients*p.nPresentations, p.nRuns);
+    Cnt=1;
+    for scan = 1:p.nRuns 
+        for pres = 1:p.nPresentations % 4 presentations in each of 8 runs, from Ho et al. 
+            if scan <= p.nRuns/2 
+                NTFresponses = splice_NTFresponses(NTFresponses_Baseline, p,sub, Cnt);
+                g(:,scan) = 1; % "accurate" trials
+            elseif scan > p.nRuns/2
+                NTFresponses = NTFresponses_Baseline;
+                g(:,scan) = 2; % "inaccurate" trials
+            end
+            rows = (pres-1)*p.nOrients+1:(pres-1)*p.nOrients+p.nOrients;
+            voxelResponses(rows,:,scan) = NTFresponses*voxelWeights; % [ p.nOrients p.nNTFs ]*[ p.nNTFs p.nVox ]
+            % gives a [nOrients*nPresentations nVox nRuns] matrix with response of every voxel to every stim
+            if scan==1
+                or(rows) = [1:9]';
+                stimPres(rows) = pres*ones(9,1);
+            end   
+            Cnt=Cnt+1;
+        end
+    end
+    
+    p.or = repmat(or,[p.nRuns 1]); % nTrials long, giving orientation of stim
+    p.stimPres = repmat(stimPres,[p.nRuns 1]); % nTrials long, giving presentation # within run
+    runs = repmat([1:p.nRuns],[p.nOrients*p.nPresentations 1]);
+    p.runs = reshape(runs, [size(runs,1)*size(runs,2) 1]);
+    p.g = reshape(g, [size(g,1)*size(g,2) 1]); %reshape to state acc versus inacc in a vector of length nTrials
+    voxResp=[];
+    for rr = 1:p.nRuns
+        voxResp = cat(1, voxResp, voxelResponses(:,:,rr));
+    end
+
+    voxelNoiseSD = mean(mean(voxResp,1))*voxNoiseScale; %take mean across all trials for each voxel
+    p.data(sub).voxelNoiseSD = voxelNoiseSD;
+    % we will assume same SD of noise for each voxel, which we will scale by their mean activation across all stimuli
+    % then sample from this noise distributions separately for each voxel, stim orient and presentation
+
+    % Liu et al. specified sigma of Gaussian noise using mean of all voxels' responses per stimulus (see generateBOLDResp)
+    % assuming same SD of noise for all voxels for a given stim orient, and then (I think) the actual noise on a given trial was sampled separately
+    % they probably didn't intend there to be big differences in noise SD between stimuli, but did it this way for convenience (because of structure of
+    % code)
+
+    p.data(sub).voxResp = voxResp + normrnd(0,voxelNoiseSD, [p.nTrials, p.nVox]);
+    
+end
+
+%% plot voxel BOLD responses for last subject
+voxResp_perOrient = zeros(p.nOrients,p.nVox);
+for orient = 1:p.nOrients
+    voxResp_perOrient(orient,:) = mean(voxResp(p.or==orient,:),1);
+end
+
+fig_num = 6;
+figure(fig_num)
+clf(fig_num)
+plot([1:p.nOrients],voxResp_perOrient(:,1))
+ylim([min(min(min(voxResp_perOrient))) max(max(max(voxResp_perOrient)))])
+hold on
+plot([1:p.nOrients],voxResp_perOrient(:,21))
+plot([1:p.nOrients],voxResp_perOrient(:,34))
+plot([1:p.nOrients],voxResp_perOrient(:,41))
+plot([1:p.nOrients],voxResp_perOrient(:,53))
+plot([1:p.nOrients],voxResp_perOrient(:,61))
+plot([1:p.nOrients],voxResp_perOrient(:,81))
+plot([1:p.nOrients],voxResp_perOrient(:,95))
+title('Voxel BOLD responses')
+
+
+%END OF parent function
+
+
+%% splice NTF responses function
+function [NTFresp] = splice_NTFresponses(NTFresp_Base, p, sub, Cnt)
+       
+    %NTFresp is [9 x 180] representing 9 orientations and 180 NTFs     
+    NTFresp = zeros(p.nOrients,p.nNTFs);
+    
+    s.x = linspace(0, pi-pi/(p.nOrients), p.nOrients);
+    s.nCenters = linspace(0, pi-pi/(p.nNTFs), p.nNTFs);
+    
+    for orient = 1:p.nOrients
+        if p.nOrients == 9
+            startPoint = [1 21 41 61 81 101 121 141 161 ];
+%             allPoints = zeros(1,180);
+            % recall that p.stimOrient = [0 20 40 60 80 100 120 140 160];
+            stim = s.x(orient);
+            
+            %Method #1
+            %construct sine wave sq profile with low points at stim orient and 90deg from stim orient (peaks in between)
+            % note, baseline here is 1 so that all NTFs have at least a gain of 1, some greater than 1
+            gainDist = p.gainFactor * sin(2*(stim - s.nCenters)).^p.notchwidth + 1;
+           
+            %find which 90 elements in the 180 element vector need to be made negative
+            findEndPoints = circshift(startPoint, -(orient-1));
+            posEndPoints = [findEndPoints(1) ;  mean(findEndPoints(5:6))];
+            if posEndPoints(1) < posEndPoints(2)
+                posPoints = [posEndPoints(1): posEndPoints(2)];
+%                 allPoints(posPoints)=1;                
+            elseif posEndPoints(1) > posEndPoints(2)
+                posPoints = [posEndPoints(1):180 1:posEndPoints(2)];
+%                 allPoints(posPoints)=1;     
+            elseif posEndPoints(1) == posEndPoints(2)
+                posPoints = [posEndPoints(1): posEndPoints(1)+90]
+%                 allPoints(posPoints)=1;
+            end
+            negPoints = ones(1,180);
+            negPoints(posPoints) = 0;
+            negPoints = find(negPoints==1);
+            
+            finalGainDist(posPoints) = gainDist(posPoints);
+            finalGainDist(negPoints) = -(gainDist(negPoints)-1)+1;
+       
+            %Method #2
+%             gainDist = abs( p.gainFactor * -((mod((stim-p.NTFcenters+pi/2),pi)-pi/2)/p.NTFsig^2) .* exp(-(mod((stim-p.NTFcenters+pi/2),pi)-pi/2).^2 / (2*p.NTFsig^2)) ) + 1;
+            
+            %this will be used to specify gain for each NTF in the voxel
+            NTFresp(orient,:) = NTFresp_Base(orient,:).*finalGainDist;
+%             stop=1
+        else
+            error('need to rewrite code to deal with nOrients not equal to 9!')
+        end  
+    end
+
+    if sub == 1 && Cnt == 1
+        fig_num = 4;
+        figure(fig_num)
+        clf(fig_num)
+        plot([1:p.nOrients],NTFresp(:,1))
+        ylim([min(min(min(NTFresp))) max(max(max(NTFresp)))])
+        hold on
+        plot([1:p.nOrients],NTFresp(:,21))
+        plot([1:p.nOrients],NTFresp(:,41))
+        plot([1:p.nOrients],NTFresp(:,61))
+        plot([1:p.nOrients],NTFresp(:,81))
+        plot([1:p.nOrients],NTFresp(:,101))
+        plot([1:p.nOrients],NTFresp(:,121))    
+        plot([1:p.nOrients],NTFresp(:,141))
+        plot([1:p.nOrients],NTFresp(:,161))
+        title('NTF responses')
+
+        fig_num=5;
+        figure(fig_num)
+        clf(fig_num)
+        imagesc(NTFresp);
+        colorbar;
+        title('Off-Chan Gained NTFs');
+
+        fig_num = 3;
+        figure(fig_num)
+        hold on 
+        plot([1:180],NTFresp(3,:))
+        legend({'NTF Baseline', 'NTF Off-Chan Gain'})
+        legend boxoff
+        
+    end
+end
+
