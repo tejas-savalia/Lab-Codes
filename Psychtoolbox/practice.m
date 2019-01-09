7746c3ec9979eefa0a54e6f3e085c19ef3df881c
tejassavalia7 <tejas.savalia7@gmail.com>
2019-01-09 17:13:09 -0500

    Trial structure done. Next up adding inter trial and inter block stuff.

--- Psychtoolbox/practice.m
+++ Psychtoolbox/practice.m
@@ -1,11 +1,12 @@
-%Trial Structure
-%Only one square at a time.
+%Trial Structure. Done.
+%Inter trial and inter block interval code.
+%Only one square at a time. Done
 %90 degree rotation.
 %Two cases: Gradual and Sudden
 %RMSE, IDE and MT
 %After effects stage
 %Auditory cue
-%Color change on hit
+%Color change on hit. Changed random squares on hit. Done.
 sca;
 close all;
 clearvars;
@@ -37,59 +38,77 @@ Screen('DrawDots', window, [xCenter, yCenter], dotSizePix, dotColor, [], 2);
 Screen('Flip', window);    
 Xs = [];
 Ys = [];
-for frame = 1:numFrames
-    Screen('FillRect', window, [0, 0.5, 0.5]);
-    [x, y, buttons] = GetMouse(window);
-    theta = atan(y-yCenter/x-xCenter);
-    r = (x-xCenter)/cos(theta);
-    %theta = acos((x - xCenter)/r);
-    
-    baseRect = [0 0 100 100];
 
-    squareTheta = [0, pi/4, pi/2, 3*pi/4, pi, 5*pi/4, 3*pi/2, 7*pi/4];
+baseRect = [0 0 100 100];
+numRects = 8;
 
-    % Screen X positions of 6 rectangles
-    squareXpos = 300*cos(squareTheta) + xCenter;
-    squareYpos = 300*sin(squareTheta) + yCenter;
-    numSqaures = length(squareXpos);
+squareTheta = [0, pi/4, pi/2, 3*pi/4, pi, 5*pi/4, 3*pi/2, 7*pi/4];
 
-    % Set the colors to Red
-    %allColors = [1 0 0; 0 1 0; 0 0 1];
-    allColors = [1 0 0];
+randomSquarePos = randi(length(squareTheta));
+randomSquareTheta = squareTheta(randomSquarePos);
+randomSquareXpos = 300*cos(randomSquareTheta) + xCenter;
+randomSquareYpos = 300*sin(randomSquareTheta) + yCenter;
 
-    % Make rectangle coordinates
-    allRects = nan(4, 8);
-    for i = 1:numSqaures
-        allRects(:, i) = CenterRectOnPointd(baseRect, squareXpos(i), squareYpos(i));
-    end
+randomSquareColor = [1, 0, 0];
+randomSquare = CenterRectOnPointd(baseRect, randomSquareXpos, randomSquareYpos);
 
-    % Draw the rect to the screen
-    Screen('FillRect', window, allColors, allRects);
+%Screen('Flip', window);
 
-    if buttons(1) 
-        HideCursor();
-        %SetMouse(x + r*cos(theta+pi/4), y + r*sin(theta+pi/4), window);
-        newX = (x-xCenter)*cos(rotateBy) + (y-yCenter)*sin(rotateBy);
-        newY = -(x-xCenter)*sin(rotateBy) + (y-yCenter)*cos(rotateBy);
-        Xs = [Xs newX];
-        Ys = [Ys newY];
-        %Change color when in rectangle. Will work with only one rectangle
-        %shown
-        if IsInRect(newX, newY, allRects)
-            allColors = [0 1 0];
-        end
+numBlocks = 1;
+numTrials = 2;
 
-
-        Screen('DrawDots', window, [xCenter+newX, yCenter+newY], dotSizePix, dotColor, [], 2);        
-    end
-    if ~buttons(1)
-        SetMouse(xCenter, yCenter, window);
-        Screen('DrawDots', window, [xCenter, yCenter], dotSizePix, dotColor, [], 2);        
-    end
+for block = 1:numBlocks
+    %Code for inter block interval
+    for trial = 1:numTrials
+        %Code for inter-trial-interval
+        for frame = 1:numFrames
         
-    Screen('Flip', window);
-    
-end
+            Screen('FillRect', window, [0, 0.5, 0.5]);
+            [x, y, buttons] = GetMouse(window);
+            theta = atan(y-yCenter/x-xCenter);
+            r = (x-xCenter)/cos(theta);
+            %theta = acos((x - xCenter)/r);
+
+            % Draw the rect to the screen
+            %Screen('FillRect', window, allColors, allRects);
+            Screen('FillRect', window, randomSquareColor, randomSquare);
+
+
+
+            if buttons(1) 
+                HideCursor();
+                %SetMouse(x + r*cos(theta+pi/4), y + r*sin(theta+pi/4), window);
+                newX = (x-xCenter)*cos(rotateBy) + (y-yCenter)*sin(rotateBy);
+                newY = -(x-xCenter)*sin(rotateBy) + (y-yCenter)*cos(rotateBy);
+                Xs = [Xs newX];
+                Ys = [Ys newY];
+                %Change color when in rectangle. Will work with only one rectangle
+                %shown
+                inside = IsInRect(newX+xCenter, newY+yCenter, randomSquare);
+                if inside
+                    %printf('here');
+                    randomSquarePos = randi(length(squareTheta));
+                    randomSquareTheta = squareTheta(randomSquarePos);
+                    randomSquareXpos = 300*cos(randomSquareTheta) + xCenter;
+                    randomSquareYpos = 300*sin(randomSquareTheta) + yCenter;
+
+                    randomSquareColor = [1, 0, 0];
+                    randomSquare = CenterRectOnPointd(baseRect, randomSquareXpos, randomSquareYpos);
+                    break;
+                end
+
+
+                Screen('DrawDots', window, [xCenter+newX, yCenter+newY], dotSizePix, dotColor, [], 2);        
+            end
+            if ~buttons(1)
+                SetMouse(xCenter, yCenter, window);
+                Screen('DrawDots', window, [xCenter, yCenter], dotSizePix, dotColor, [], 2);        
+            end
+
+            Screen('Flip', window);
+        end
+    end
+ end
 %SetMouse(400, 400, window)
 KbStrokeWait;
 
