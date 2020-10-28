cd139e0aa66bc573e5d637723b6abd1f37240cd9
tejassavalia7 <tejas.savalia7@gmail.com>
2020-10-28 19:36:45 -0400

    Rerun fit routines

--- Psychtoolbox/Visuomotor_Adaptation_Tablet/python_scripts/dual_model_with_transfer.py
+++ Psychtoolbox/Visuomotor_Adaptation_Tablet/python_scripts/dual_model_with_transfer.py
@@ -180,12 +180,12 @@ def main():
     #if __name__ == '__main__':
     #its = pickle.load(open('its.pickle', 'rb'))
     #mts = pickle.load(open('mts.pickle', 'rb'))
-    #curvatures_smooth = pickle.load(open('curvatures.pickle', 'rb'))
-    curvatures_smooth = pickle.load(open('generated_by_dual.pickle', 'rb'))
+    curvatures_smooth = pickle.load(open('curvatures_smooth.pickle', 'rb'))
+    #curvatures_smooth = pickle.load(open('generated_by_dual.pickle', 'rb'))
     #curvatures_smooth = pickle.load(open('single_with_transfer_generated_errors.pickle', 'rb'))
     #total_time = its+mts
     #Test git and vscode 
-    #curvatures_smooth = curvatures_smooth/90.0
+    curvatures_smooth = curvatures_smooth/90.0
     #curvatures_smooth = gaussian_filter1d(total_time, 2)
     #curvatures_smooth = curvatures_smooth/np.max(curvatures_smooth)
     #print("parallel curvatures successful")
@@ -199,7 +199,7 @@ def main():
     #%% Parallel run and dump fits
     fits = run_fits_dual(curvatures_smooth, 640, 640)
     #with open('fit_dual_bound_with_transfer_model_recovery.pickle', 'wb') as f:
-    with open('fit_dual_bound_with_transfer_GBD.pickle', 'wb') as f:
+    with open('fit_dual_bound_with_transfer.pickle', 'wb') as f:
         
         pickle.dump(fits, f)
     f.close()
