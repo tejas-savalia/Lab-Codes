dc0f0b9d10adb97557c30fa9e445c0c4bc14f98b
tejassavalia7 <tejas.savalia7@gmail.com>
2021-02-16 12:58:09 -0500

    Almost final results implemented

--- Visuomotor_Adaptation_psychopy_tablet/no_feedback/analysis/ddms.py
+++ Visuomotor_Adaptation_psychopy_tablet/no_feedback/analysis/ddms.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,11 +15,18 @@ from ddm import Fittable
 from ddm.models import LossRobustBIC, LossRobustLikelihood, LossSquaredError
 from ddm.functions import fit_adjust_model, fit_model
 import seaborn as sns
-from ddm import Bound
-from ddm import Drift
-from multiprocessing import Pool
-import pickle
 
+
+# In[2]:
+
+
+from ddm import set_N_cpus
+set_N_cpus(8)
+
+
+# In[ ]:
+
+from ddm import Bound
 class BoundBlocks(Bound):
     name = "constant"
     required_parameters = ["BBlock0", "BBlock1", "BBlock2", "BBlock3", "BBlock4", "BBlock5",
@@ -45,12 +58,16 @@ class BoundBlocks(Bound):
         if conditions['block'] == 11:
             return self.BBlock11
 
+
+# In[48]:
+
+
+from ddm import Drift
 class DriftBlocks(Drift):
     name = "constant"
     required_parameters = ["VBlock0", "VBlock1", "VBlock2", "VBlock3", "VBlock4", "VBlock5",
                           "VBlock6", "VBlock7", "VBlock8", "VBlock9", "VBlock10", "VBlock11"]
     required_conditions = ['block']
-
     def get_drift(self, conditions, *args, **kwargs):
         if conditions['block'] == 0:
             return self.VBlock0
@@ -77,10 +94,16 @@ class DriftBlocks(Drift):
         if conditions['block'] == 11:
             return self.VBlock11
 
-def run_ddm_fits(participant):
+
+# In[65]:
+
+
+model_fits = np.zeros(14, dtype = object)
+for participant in range(14):
     print(participant)
     df_rt = pd.read_csv(open('RTs.csv'))
     df_error = pd.read_csv(open('Angular_errors.csv'))
+
     df_rt['Correct'] = 0
     df_rt['Correct'][df_error['Error'] < np.pi/9] = 1
     df_rt['Correct'][df_error['Error'] > np.pi/9] = 0
@@ -92,50 +115,45 @@ def run_ddm_fits(participant):
     df_rt = df_rt.drop(['Trial', 'Unnamed: 0', 'participant_id', 'condition', 'MTs'], axis = 1)
 
     samp = Sample.from_pandas_dataframe(df_rt, rt_column_name="ITs", correct_column_name="Correct")
-    try:
-        model_fits = fit_model(samp, 
-                               bound=BoundBlocks(BBlock0 = Fittable(minval = 0, maxval = 20),
-                                                 BBlock1 = Fittable(minval = 0, maxval = 20),
-                                                 BBlock2 = Fittable(minval = 0, maxval = 20),
-                                                 BBlock3 = Fittable(minval = 0, maxval = 20),
-                                                 BBlock4 = Fittable(minval = 0, maxval = 20),
-                                                 BBlock5 = Fittable(minval = 0, maxval = 20),
-                                                 BBlock6 = Fittable(minval = 0, maxval = 20),
-                                                 BBlock7 = Fittable(minval = 0, maxval = 20),
-                                                 BBlock8 = Fittable(minval = 0, maxval = 20),
-                                                 BBlock9 = Fittable(minval = 0, maxval = 20),
-                                                 BBlock10 = Fittable(minval = 0, maxval = 20),
-                                                 BBlock11 = Fittable(minval = 0, maxval = 20)),
-                               drift=DriftBlocks(VBlock0 = Fittable(minval = -20, maxval = 20),
-                                                 VBlock1 = Fittable(minval = -20, maxval = 20),
-                                                 VBlock2 = Fittable(minval = -20, maxval = 20),
-                                                 VBlock3 = Fittable(minval = -20, maxval = 20),
-                                                 VBlock4 = Fittable(minval = -20, maxval = 20),
-                                                 VBlock5 = Fittable(minval = -20, maxval = 20),
-                                                 VBlock6 = Fittable(minval = -20, maxval = 20),
-                                                 VBlock7 = Fittable(minval = -20, maxval = 20),
-                                                 VBlock8 = Fittable(minval = -20, maxval = 20),
-                                                 VBlock9 = Fittable(minval = -20, maxval = 20),
-                                                 VBlock10 = Fittable(minval = -20, maxval = 20),
-                                                 VBlock11 = Fittable(minval = -20, maxval = 20)),
-                               overlay=OverlayNonDecision(nondectime=Fittable(minval=0, maxval=0.5)),
-
-                               noise=NoiseConstant(noise=Fittable(minval=0.01, maxval=5)),
-
-                               lossfunction=LossRobustLikelihood, 
-                               verbose = False
-                              )
-        print("Participant done: ", participant)
-    except:
-        print("participant failed: ", participant)
-        model_fits = 0
-    return model_fits
     
-def main():
-        pool = Pool()
-        model_fits = np.array(pool.map(run_ddm_fits, range(14)))
-        pickle.dump(model_fits, open('pyddm_fits.pickle', 'wb'))
+    model_fits[participant] = fit_model(samp, bound=BoundBlocks(BBlock0 = Fittable(minval = 0, maxval = 20),
+                               BBlock1 = Fittable(minval = 0, maxval = 20),
+                               BBlock2 = Fittable(minval = 0, maxval = 20),
+                               BBlock3 = Fittable(minval = 0, maxval = 20),
+                               BBlock4 = Fittable(minval = 0, maxval = 20),
+                               BBlock5 = Fittable(minval = 0, maxval = 20),
+                               BBlock6 = Fittable(minval = 0, maxval = 20),
+                               BBlock7 = Fittable(minval = 0, maxval = 20),
+                               BBlock8 = Fittable(minval = 0, maxval = 20),
+                               BBlock9 = Fittable(minval = 0, maxval = 20),
+                               BBlock10 = Fittable(minval = 0, maxval = 20),
+                               BBlock11 = Fittable(minval = 0, maxval = 20)),
+              
+              drift=DriftBlocks(VBlock0 = Fittable(minval = -20, maxval = 20),
+                               VBlock1 = Fittable(minval = -20, maxval = 20),
+                               VBlock2 = Fittable(minval = -20, maxval = 20),
+                               VBlock3 = Fittable(minval = -20, maxval = 20),
+                               VBlock4 = Fittable(minval = -20, maxval = 20),
+                               VBlock5 = Fittable(minval = -20, maxval = 20),
+                               VBlock6 = Fittable(minval = -20, maxval = 20),
+                               VBlock7 = Fittable(minval = -20, maxval = 20),
+                               VBlock8 = Fittable(minval = -20, maxval = 20),
+                               VBlock9 = Fittable(minval = -20, maxval = 20),
+                               VBlock10 = Fittable(minval = -20, maxval = 20),
+                               VBlock11 = Fittable(minval = -20, maxval = 20)),
+              
+              overlay=OverlayNonDecision(nondectime=Fittable(minval=0, maxval=0.5)),
+              
+              noise=NoiseConstant(noise=Fittable(minval=0.01, maxval=5)), 
+              lossfunction=LossRobustLikelihood, verbose = False, dt = 0.0001
+             )
 
 
-if __name__ == '__main__':
-    main()
+# In[74]:
+
+
+import pickle
+pickle.dump(model_fits, open('ddm_model_point_fits.pickle', 'wb'))
+
+
+
