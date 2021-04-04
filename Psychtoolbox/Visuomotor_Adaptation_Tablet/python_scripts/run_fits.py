9cd49ef9260b3c311618f4202987c5e2244aeb48
vm01 <tejas.savalia7@gmail.com>
2021-04-04 19:50:30 -0400

    ide single done

--- Psychtoolbox/Visuomotor_Adaptation_Tablet/python_scripts/run_fits.py
+++ Psychtoolbox/Visuomotor_Adaptation_Tablet/python_scripts/run_fits.py
@@ -18,42 +18,42 @@ def main(num_fit_trials):
     #curvatures_smooth = pickle.load(open('curvatures_smooth.pickle', 'rb'))
     #curvatures_smooth = curvatures_smooth/90
 
-    curvatures_smooth = pickle.load(open('auc_smooth.pickle', 'rb'))
-    curvatures_smooth = curvatures_smooth/np.nanmax(curvatures_smooth)    
+    #curvatures_smooth = pickle.load(open('auc_smooth.pickle', 'rb'))
+    #curvatures_smooth = curvatures_smooth/np.nanmax(curvatures_smooth)    
     
-    print ("AUC Curvatures Loaded. In Fit routine")
-    print (num_fit_trials)
+    #print ("AUC Curvatures Loaded. In Fit routine")
+    #print (num_fit_trials)
 
     #%% Parallel run and dump fits
     
-    fits = run_fits_single(curvatures_smooth, int(num_fit_trials[1]), int(num_fit_trials[2]))
-    with open('fit_single_CV_704_auc.pickle', 'wb') as f:
-        pickle.dump(fits, f)
-    f.close()
+    #fits = run_fits_single(curvatures_smooth, int(num_fit_trials[1]), int(num_fit_trials[2]))
+    #with open('fit_single_CV_704_auc.pickle', 'wb') as f:
+    #    pickle.dump(fits, f)
+    #f.close()
     
-    fits = run_fits_dual(curvatures_smooth, int(num_fit_trials[1]), int(num_fit_trials[2]))
-    with open('fit_dual_CV_704_auc.pickle', 'wb') as f:
-        pickle.dump(fits, f)
-    f.close()
+    #fits = run_fits_dual(curvatures_smooth, int(num_fit_trials[1]), int(num_fit_trials[2]))
+    #with open('fit_dual_CV_704_auc.pickle', 'wb') as f:
+    #    pickle.dump(fits, f)
+    #f.close()
 
-    curvatures_smooth = pickle.load(open('mad_smooth.pickle', 'rb'))
-    curvatures_smooth = curvatures_smooth/np.nanmax(curvatures_smooth)
+    #curvatures_smooth = pickle.load(open('mad_smooth.pickle', 'rb'))
+    #curvatures_smooth = curvatures_smooth/np.nanmax(curvatures_smooth)
 
 
-    print ("MAD Curvatures Loaded. In Fit routine")
-    print (num_fit_trials)
+    #print ("MAD Curvatures Loaded. In Fit routine")
+    #print (num_fit_trials)
 
     #%% Parallel run and dump fits
     
-    fits = run_fits_single(curvatures_smooth, int(num_fit_trials[1]), int(num_fit_trials[2]))
-    with open('fit_single_CV_704_mad.pickle', 'wb') as f:
-        pickle.dump(fits, f)
-    f.close()
+    #fits = run_fits_single(curvatures_smooth, int(num_fit_trials[1]), int(num_fit_trials[2]))
+    #with open('fit_single_CV_704_mad.pickle', 'wb') as f:
+    #    pickle.dump(fits, f)
+    #f.close()
     
-    fits = run_fits_dual(curvatures_smooth, int(num_fit_trials[1]), int(num_fit_trials[2]))
-    with open('fit_dual_CV_704_mad.pickle', 'wb') as f:
-        pickle.dump(fits, f)
-    f.close()
+    #fits = run_fits_dual(curvatures_smooth, int(num_fit_trials[1]), int(num_fit_trials[2]))
+    #with open('fit_dual_CV_704_mad.pickle', 'wb') as f:
+    #    pickle.dump(fits, f)
+    #f.close()
 
     
     curvatures_smooth = pickle.load(open('ide_smooth.pickle', 'rb'))
@@ -64,10 +64,10 @@ def main(num_fit_trials):
 
     #%% Parallel run and dump fits
     
-    fits = run_fits_single(curvatures_smooth, int(num_fit_trials[1]), int(num_fit_trials[2]))
-    with open('fit_single_CV_704_ide.pickle', 'wb') as f:
-        pickle.dump(fits, f)
-    f.close()
+    #fits = run_fits_single(curvatures_smooth, int(num_fit_trials[1]), int(num_fit_trials[2]))
+    #with open('fit_single_CV_704_ide.pickle', 'wb') as f:
+    #    pickle.dump(fits, f)
+    #f.close()
     
     fits = run_fits_dual(curvatures_smooth, int(num_fit_trials[1]), int(num_fit_trials[2]))
     with open('fit_dual_CV_704_ide.pickle', 'wb') as f:
