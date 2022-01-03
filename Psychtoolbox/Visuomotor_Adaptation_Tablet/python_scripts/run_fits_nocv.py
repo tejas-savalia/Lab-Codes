52f770ce51e6488f4a8844917628cff6a231c3a1
tejassavalia7 <tejas.savalia7@gmail.com>
2022-01-03 12:32:04 -0500

    running fit routines without cv, simplex, random start

--- Psychtoolbox/Visuomotor_Adaptation_Tablet/python_scripts/run_fits_nocv.py
+++ Psychtoolbox/Visuomotor_Adaptation_Tablet/python_scripts/run_fits_nocv.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter1d
 from all_models import *
 from fits_single_param_start import *
+from fits_no_cv import *
 #from transfer_models import *
 import sys
 
@@ -20,8 +21,8 @@ def main(num_fit_trials):
     curvatures_smooth = curvatures_smooth/90.0
     #curvatures_smooth = pickle.load(open('mean_drawn_grouperrors.pickle', 'rb'))
     
-    fits = run_fits_singlestart(curvatures_smooth, int(num_fit_trials[1]))
-    with open('fit_single_start_640.pickle', 'wb') as f:
+    fits = run_fits_nocv(curvatures_smooth, int(num_fit_trials[1]))
+    with open('fit_nocv_640.pickle', 'wb') as f:
         pickle.dump(fits, f)
     f.close()
     #fits = run_fits_single(curvatures_smooth, int(num_fit_trials[1]), int(num_fit_trials[2]), int(num_fit_trials[3]))
